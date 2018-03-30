79110ba3c79b0c7cb3647056d14801a424b6ad1f
ana.buigues <ana.buigues@ana.local>
2018-03-31 00:21:23 +0200

    simple controller

---
+++ web-services/src/main/java/com/anabuigues/webservices/webservices/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.anabuigues.webservices.webservices;
+
+public class HelloWorldBean {
+	
+	private String message;
+
+	public HelloWorldBean(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}	
+}
